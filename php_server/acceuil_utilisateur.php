96dcee6cd4ae6fb2b101979bffd3f676abf1020a
root <root@localhost.localdomain>
2018-12-05 00:27:40 +0100

    follow up

--- php_server/acceuil_utilisateur.php
+++ php_server/acceuil_utilisateur.php
@@ -95,6 +95,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="./style1.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 		<title>Acceuil</title>
 	</head>
 	<body>
